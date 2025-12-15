92d1a9efe429a1a5d34e32de19af3bfbdfe5ff51
Ali hrera <alihrhera1@gmail.com>
2025-12-15 03:53:49 +0200

    Implement file splitting logic in `FileSeparatorUtil`
    - Added `splitFileToChach` method to split an input file into chunks and save them to the cache directory.
    - Implemented logic to write chunked files (e.g., `filename.part1`, `filename.part2`) and create an `info.json` file describing the parts.
    - Added `chunkSizeInByte` helper method to convert MB to bytes.
    - Utilized Gson to serialize `ItemFileInfo` metadata.

--- app/src/main/java/hrhera/ali/backgroundsync/util/FileSeparatorUtil.kt
+++ app/src/main/java/hrhera/ali/backgroundsync/util/FileSeparatorUtil.kt
@@ -1,8 +1,58 @@
 package hrhera.ali.backgroundsync.util
 
+import android.content.Context
+import com.google.gson.Gson
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+
 
 object FileSeparatorUtil {
+    fun splitFileToChach(
+        context: Context,
+        inputFile: File,
+        itemId: String,
+        chunkSize: Int
+    ): ItemFileInfo {
 
+        val itemCacheDir = File(context.cacheDir, itemId)
+        if(!itemCacheDir.exists())itemCacheDir.mkdirs()
+        val jsonFile = File(itemCacheDir, "info.json")
+
+        val buffer = ByteArray(chunkSizeInByte(chunkSize).toInt())
+        val partsMap = mutableMapOf<Int, String>()
+        FileInputStream(inputFile).use { inputStream ->
+            var bytesRead: Int
+            var index = 0
+            while (inputStream.read(buffer).also { bytesRead = it } > 0) {
+                val chunkFile = File(
+                    itemCacheDir,
+                    "${inputFile.name}.part${index+1}"
+                )
+                FileOutputStream(chunkFile).use { outputStream ->
+                    outputStream.write(buffer, 0, bytesRead)
+                }
+                partsMap[index] = chunkFile.absolutePath
+                index++
+            }
+        }
+
+        val newItemInfo = ItemFileInfo(
+            itemName =itemId,
+            folderName = itemCacheDir.name,
+            parts = partsMap,
+            orignalFilePath = inputFile.absolutePath,
+            chankSizeInMb = chunkSize
+        )
+        jsonFile.writeText(Gson().toJson(newItemInfo))
+
+        return newItemInfo
+    }
+
+
+    private fun chunkSizeInByte(chunkSizeInMb: Int): Long {
+        return (chunkSizeInMb * 1024 * 1024).toLong()
+    }
 
 }
 
